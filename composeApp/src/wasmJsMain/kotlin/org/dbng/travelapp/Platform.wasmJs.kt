a66847a963712696c1234ca9d7995354c32cd6ac
nagarajudbng <nagaraj.dbng@gmail.com>
2024-11-21 23:29:21 +0530

    added new project from kotlin multiplatform wizard

---
+++ composeApp/src/wasmJsMain/kotlin/org/dbng/travelapp/Platform.wasmJs.kt
@@ -0,0 +1,7 @@
+package org.dbng.travelapp
+
+class WasmPlatform: Platform {
+    override val name: String = "Web with Kotlin/Wasm"
+}
+
+actual fun getPlatform(): Platform = WasmPlatform()
--- shared/src/commonMain/kotlin/com/example/kotlinmultiplatformproject/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.example.kotlinmultiplatformproject
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
